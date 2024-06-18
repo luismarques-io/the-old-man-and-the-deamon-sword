35e75b6c6d0a5a588207a1b4ddce2d39cfea3613
Luís Marques <euluismarques@gmail.com>
2024-06-18 16:28:50 +0100

    Remove title from sponsors

--- src/components/sections/sponsors.tsx
+++ src/components/sections/sponsors.tsx
@@ -8,12 +8,8 @@ const Sponsors = () => {
 	const locale = useLocale();
 
 	return (
-		<section className="page-section pb-3" id="sponsors">
+		<section className="page-section pb-3 pt-4" id="sponsors">
 			<div className="container">
-				<div className="text-center">
-					<h2 className="section-heading text-uppercase">{t("title")}</h2>
-				</div>
-
 				<div className="row">
 					<div className="col-md-12">
 						<Image src={`/images/sponsors-and-credits-${locale}.png`} alt={t("image-alt-text")} width={3400} height={4961} style={{ width: "100%", height: "auto" }} />
