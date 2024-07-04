bc2a84178cf407c54cdacd47a31eac8746684a8a
Luís Marques <euluismarques@gmail.com>
2024-07-04 11:19:28 +0100

    Update fantasia link

--- src/components/ui/prizes.tsx
+++ src/components/ui/prizes.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 const Prizes = () => {
 	return (
 		<div className="text-center">
-			<a href="https://fantasiafestival.com/" className="btn-prize" target="_blank" rel="noopener noreferrer">
+			<a href="https://fantasiafestival.com/en/film/the-old-man-and-the-demon-sword" className="btn-prize" target="_blank" rel="noopener noreferrer">
 				<Image
 					src={`/images/prizes/Fantasia2024-OfficialSelection-EN-darkmode.png`}
 					alt="Fantasia2024 OfficialSelection"
